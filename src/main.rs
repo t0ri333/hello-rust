545b32da9582736d74aaf97126701dbf02a8c075
tori <ziliangz@gmail.com>
2023-02-15 18:53:12 -0800

    make my first rust server

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,10 @@
-use ferris_says::say;
-use std::io::{stdout, BufWriter};
+extern crate iron;
+
+use iron::prelude::*;
+use iron::status;
 
 fn main() {
-    let stdout = stdout();
-    let message = String::from("Hello fellow Rustaceans!");
-    let width = message.chars().count();
-
-    let mut writer = BufWriter::new(stdout.lock());
-    say(message.as_bytes(),  width, &mut writer).unwrap();
+    Iron::new(|_: &mut Request| {
+        Ok(Response::with((status::Ok, "Hello, world!")))
+    }).http("localhost:3001").unwrap();
 }
-
